25ef704ec65ccd9b9b77c03dd00286c34eeb4b9f
DJRT-11 <138712641+DJRT-11@users.noreply.github.com>
2023-11-13 17:35:05 -0500

    Upload files

---
+++ data_model.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class DataModel(BaseModel):
+    gender: str
+    SeniorCitizen: str
+    Partner: str
+    Dependents: str
+    tenure: int
+    PhoneService: str
+    MultipleLines: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
+    MonthlyCharges: float
+    TotalCharges: float
